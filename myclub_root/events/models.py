f5545f1cfa73c347ef0de5a8e956440972788cbc
Venus <valievavm@gmail.com>
2020-04-16 23:58:45 +0200

    Add Venue & MyclubUser models, view with calendar

--- myclub_root/events/models.py
+++ myclub_root/events/models.py
@@ -1,13 +1,43 @@
 from django.db import models
 
 
-class Events(models.Model):
+class Venue(models.Model):
+    """
+    Model for venues
+    """
+    name = models.CharField('Venue Name', max_length=120)
+    address = models.CharField('Venue Address', max_length=300)
+    zip_code = models.CharField('Venue Zip/Post Code', max_length=12)
+    phone = models.CharField('Venue phone', max_length=20)
+    website = models.URLField('Venue URL')
+    email = models.EmailField('Venue Email')
+
+    def __str__(self):
+        return self.name
+
+
+class MyclubUser(models.Model):
+    """
+    Model for Myclub users
+    """
+    first_name = models.CharField('Myclub user first name', max_length=30)
+    last_name = models.CharField('Myclub user last name', max_length=30)
+    email = models.EmailField('Myclub user email')
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Event(models.Model):
     """
     Model for events
     """
-    name = models.CharField(verbose_name='Event name', max_length=120)
-    date = models.DateTimeField(verbose_name='Event date')
-    venue = models.CharField(verbose_name='Event venue', max_length=120)
-    manager = models.CharField(verbose_name='Event manager', max_length=120)
-    description = models.TextField(verbose_name='Event description', blank=True)  # can be empty
+    name = models.CharField('Event name', max_length=120)
+    date = models.DateTimeField('Event date')
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    manager = models.CharField('Event manager', max_length=120)
+    attendees = models.ManyToManyField(MyclubUser)
+    description = models.TextField('Event description', blank=True)
 
+    def __str__(self):
+        return self.name
--- myclub_root/events/views.py
+++ myclub_root/events/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import date
+import calendar
+from calendar import HTMLCalendar
 
-# Create your views here.
+
+def index(request, year=date.today().year, month=date.today().month):
+    year = int(year)
+    month = int(month)
+    if year < 2000 or year > 2099:
+        year = date.today().year
+    month_str = calendar.month_name[month]
+    title = f'Myclub Event Calendar - {month_str}, {year}'
+    cal = HTMLCalendar().formatmonth(year, month)
+
+    return HttpResponse(f'<h1>{title}</h1>'
+                        f'<p>{cal}</p>')
