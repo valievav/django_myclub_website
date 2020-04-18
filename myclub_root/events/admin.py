cf58907360952147fb18a0d3633eda37ffec03c4
Venus <valievavm@gmail.com>
2020-04-18 13:42:59 +0200

    Add models to Admin, models customization

--- myclub_root/events/admin.py
+++ myclub_root/events/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Venue, MyclubUser, Event
 
-# Register your models here.
+
+@admin.register(Venue)  # register custom ModelAdmin subclass (instead of admin.site.register(Venue, VenueAdmin))
+class VenueAdmin(admin.ModelAdmin):
+    fields = ('name', ('address', 'zip_code'), 'website', ('phone', 'email'))  # display some fields inline (edit form)
+    list_display = ('name', 'address', 'website', 'phone')  # display fields in all items list
+    ordering = ('name',)
+    search_fields = ('name', 'address')
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date', 'venue', 'manager')  # displayed in all items list
+    list_filter = ('name', 'date', 'venue')
+    ordering = ('-date',)
+    search_fields = ('name', 'venue')
+    fieldsets = (
+        ('Required Information', {'description': "These fields are required for each event",
+                                  'fields': (('name', 'venue'), 'date')}),
+        ('Optional Information', {'classes': ('expand',),  # to display optional section
+                                  'fields': ('description', 'manager')}),
+    )   # display fields in sections (edit form)
+
+
+admin.site.register(MyclubUser)
--- myclub_root/events/models.py
+++ myclub_root/events/models.py
@@ -1,25 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Venue(models.Model):
-    """
-    Model for venues
-    """
     name = models.CharField('Venue Name', max_length=120)
     address = models.CharField('Venue Address', max_length=300)
-    zip_code = models.CharField('Venue Zip/Post Code', max_length=12)
-    phone = models.CharField('Venue phone', max_length=20)
-    website = models.URLField('Venue URL')
-    email = models.EmailField('Venue Email')
+    zip_code = models.CharField('Venue Zip/Post Code', max_length=12, blank=True)
+    phone = models.CharField('Venue phone', max_length=20, blank=True)   # blank=True for optional field
+    website = models.URLField('Venue URL', blank=True)
+    email = models.EmailField('Venue Email', blank=True)
 
     def __str__(self):
         return self.name
 
 
 class MyclubUser(models.Model):
-    """
-    Model for Myclub users
-    """
     first_name = models.CharField('Myclub user first name', max_length=30)
     last_name = models.CharField('Myclub user last name', max_length=30)
     email = models.EmailField('Myclub user email')
@@ -29,13 +24,10 @@ class MyclubUser(models.Model):
 
 
 class Event(models.Model):
-    """
-    Model for events
-    """
     name = models.CharField('Event name', max_length=120)
     date = models.DateTimeField('Event date')
     venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
-    manager = models.CharField('Event manager', max_length=120)
+    manager = models.ForeignKey(User, blank=True, on_delete=models.SET_NULL, null=True)
     attendees = models.ManyToManyField(MyclubUser)
     description = models.TextField('Event description', blank=True)
 
