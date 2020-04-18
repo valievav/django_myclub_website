feb40d2ecd8b3b758d2182ec09b0830ac157c216
Venus <valievavm@gmail.com>
2020-04-18 14:01:26 +0200

    Add reset password to login page

--- myclub_root/myclub_site/urls.py
+++ myclub_root/myclub_site/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # to add reset password link (name should be 'admin_password_reset'!)
+    path('admin/password_reset/', auth_views.PasswordResetView.as_view(), name='admin_password_reset'),
+    path('admin/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('', include('events.urls')),
 ]
